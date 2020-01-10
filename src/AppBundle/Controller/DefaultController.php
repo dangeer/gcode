1fb643b1ca182b10e2a80cfee657e9e64fd566d6
RAOILISON Manitra Berhton <berthondanger@gmail.com>
2020-01-10 14:26:15 +0300

    mail

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,29 +11,54 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends Controller
 {
 
-	public function __construct(){
-
-	}
+    public function __construct()
+    {
+    }
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
         return $this->render('gcode/accueil.html.twig');
-	 //return $this->render('gcode/homepage.html.twig');
+        //return $this->render('gcode/homepage.html.twig');
     }
-    
+
     /**
      * @Route("/contact", name="contactPage")
      */
-	public function contact(Request $request){
+    public function contact(Request $request)
+    {
         return $this->render('gcode/contactPage.html.twig');
     }
 
     /**
      * @Route("/accueil", name="accueilpage")
      */
-    public function accueil(Request $request){
+    public function accueil(Request $request)
+    {
+        return $this->render('gcode/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/sendmail", name="sendmail")
+     */
+    public function mailAction()
+    {
+        $name='Berthon';
+        $mailer = $this->get('mailer');
+        $message = (new \Swift_Message('GCODE MAIL'))
+            ->setFrom('etech.berthon@gmail.com')
+            ->setTo('berthondanger@gmai.com')
+            ->setBody(
+                $this->renderView(
+                    'Emails/registration.html.twig',
+                    ['name' => $name]
+                ),
+                'text/html'
+            );
+
+        $mailer->send($message);
+
         return $this->render('gcode/accueil.html.twig');
     }
 }
